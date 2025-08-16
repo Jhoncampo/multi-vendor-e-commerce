772ad4bf4ebe73ff135e23e5643fe44ec3aa0e24
Eduard <eduardcamayo13@gmail.com>
2025-08-15 22:05:59 -0500

    Add navbar

---
+++ src/app/(home)/navbar.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import {Poppins} from "next/font/google"
+import Link from "next/link";
+
+const poppins = Poppins({ subsets: ["latin"], weight: ["700"] })
+
+const NavBarItem = () => {
+    return(
+        <Button>hola</Button>
+    )
+}
+
+const Navbar = () => {
+  return (
+    <nav className="h-20 flex border-b justify-between font-medium bg-white">
+      <Link href="/" className="pl-6 flex items-center">
+        <span className={cn("text-5xl font-semibold", poppins.className)}>Foundroad</span>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
