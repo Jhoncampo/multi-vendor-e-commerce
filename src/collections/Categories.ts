a6da45f9f1ccfd5e4c5aaced1d79aed665fa34b2
Eduard <eduardcamayo13@gmail.com>
2025-08-19 12:37:22 -0500

    Add something new

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,6 +1,6 @@
 import type { CollectionConfig } from "payload";
 
-export const Categories: CollectionConfig = {
+const Categories: CollectionConfig = {
     slug: 'categories',
     fields: [
         {
@@ -8,5 +8,31 @@ export const Categories: CollectionConfig = {
             type: "text",
             required: true
         },
+        {
+            name: "slug",
+            type: "text",
+            required: true,
+            unique: true,
+            index: true,
+        },
+        {
+            name: "color",
+            type: "text",
+        },
+        {
+            name: "parent",
+            type: "relationship",
+            relationTo: "categories",
+            hasMany: false,
+        },
+        {
+            name: "subcategories",
+            type: "join",
+            collection: "categories",
+            on: "parent",
+            hasMany: true
+        }
     ]
 }
+
+export default Categories;
